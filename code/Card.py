2684657141fbde3c7faa5b87a6725b1413b42429
mmanopoli <matthew.manopoli@gmail.com>
2020-01-05 23:30:28 +0000

    Building the Deck class.  Unit tests temporarily in if __name__ == '__main__' block.

--- code/Card.py
+++ code/Card.py
@@ -1,35 +1,35 @@
 class Card:
 
     # Private (name scrambled) list of valid card values
-    __valid_values = [
+    valid_values = [
         'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen',
         'King'
     ]
 
     # Private (name scrambled) list of valid card suits
-    __valid_suits = ['hearts', 'spades', 'clubs', 'diamonds']
+    valid_suits = ['hearts', 'spades', 'clubs', 'diamonds']
 
     def __init__(self, suit, value):
         # Validate and set the card's suit
-        if suit in self.__valid_suits:
+        if suit in self.valid_suits:
             self.suit = suit
         else:
             raise IllegalCardSuitError("A playing card must have one of the "
                                         "following suits: " + ', '.join(
-                                            self.__valid_suits
+                                            self.valid_suits
                                   ))
 
         # Validate and set the card's value
-        if value in self.__valid_values:
+        if value in self.valid_values:
             self.value = value
         else:
             raise IllegalCardValueError("A playing card must have one of the "
                                         "following values: " + ', '.join(
-                                            self.__valid_values
+                                            self.valid_values
                                         ))
 
     def __repr__(self):
-        return (f"value: {self.value}, suit: {self.suit}")
+        return (f"(value: {self.value}, suit: {self.suit})")
 
     def __str__(self):
         return (f"The {self.value} of {self.suit}")
---
+++ code/Deck.py
@@ -0,0 +1,33 @@
+from Card import Card
+
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        self.generate_new_deck()
+
+    def generate_new_deck(self):
+        for suit in Card.valid_suits:
+            for value in Card.valid_values:
+                self.cards.append(Card(suit, value))
+
+    def shuffle(self):
+        # TODO:
+        pass
+
+    def dealOneCard(self):
+        # TODO:
+        pass
+
+    def __str__(self):
+        return f"This is a {len(self.cards)} card deck."
+
+    def __repr__(self):
+        return f"{len(self.cards)} cards: {self.cards}"
+
+
+if __name__ == '__main__':
+    deck = Deck()
+    print(deck)
+    print(repr(deck))
